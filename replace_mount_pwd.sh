c5db2faedfb7a501a1e7f238d99524a6e4c62fe4
isandratskiy <i.sandrackii@gmail.com>
2020-03-30 01:54:15 +0300

    fix

--- replace_mount_pwd.sh
+++ replace_mount_pwd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #sed -i '' "s/mount/$(pwd)/g"
 WORKDIR=$(pwd)
-CONFIG="./browsers.json"
-find "${CONFIG}" -type f -exec sed -i "" -e "s|mount|${WORKDIR}|g" {} \;
+sed -i "" -e "s|mount|${WORKDIR}|g" "browsers.json"
 cat browsers.json
